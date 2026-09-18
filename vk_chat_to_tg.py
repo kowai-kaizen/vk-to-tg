a9ce9b3c86a6404caad8509ae005607248706756
kaizen <106261494+kowai-kaizen@users.noreply.github.com>
2026-09-18 13:52:45 +0300

    Update vk_chat_to_tg.py

--- vk_chat_to_tg.py
+++ vk_chat_to_tg.py
@@ -41,14 +41,14 @@ except ImportError:
 
 # ---------- НАСТРОЙКИ ----------
 
-VK_PEER_ID = 2000000001                 # peer_id беседы (2000000000 + chat_id)
+VK_PEER_ID = 2000000002                 # peer_id беседы (2000000000 + chat_id)
 VK_GROUP_ID = int(os.getenv("VK_GROUP_ID", "0"))  # числовой ID сообщества (положительный)
 VK_API_VERSION = "5.199"
 VK_TOKEN = os.getenv("VK_TOKEN")
 
 TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
 TG_CHAT_ID = os.getenv("TG_CHAT_ID")
-TG_TOPIC_ID = 14                          # id топика (темы) в группе; None, если топики не используются
+TG_TOPIC_ID = 4607                          # id топика (темы) в группе; None, если топики не используются
 
 STATE_FILE = "last_ts.json"               # тут храним курсор ts LongPoll
 
